ddbb2701034a1c3d9b603a2c45277716e8fe07a0
chiraanthps-dot <chiraanthps@gmail.com>
2026-05-24 16:58:19 +0530

    test

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://mockproject.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://oarzwsdbaexuehsztoxu.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_qOIQSms5QY6paRnPwdyglA_7y8S1s0G";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
